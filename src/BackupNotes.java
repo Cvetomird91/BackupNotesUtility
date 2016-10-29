24dabaaad408dd2bdcd9976c587a07bfd294a6ac
Cvetomird91 <cvetomirdenchev@gmail.com>
2016-10-30 01:55:06 +0300

    initial commit

---
+++ src/BackupNotes.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class BackupNotes {
+	
+	//private String configFilePath;
+	
+	//http://stackoverflow.com/questions/19674951/reading-ini-file-in-java
+	//properties syntax: https://docs.oracle.com/cd/E23095_01/Platform.93/ATGProgGuide/html/s0204propertiesfileformat01.html
+	
+	//java.util.prefs.Preferences;
+	//new Properties properties = rowProperties.load(new FileInputStream());
+	
+	//http://stackoverflow.com/questions/367706/how-to-parse-command-line-arguments-in-java
+	
+	private String BackupsDir;
+	private String pCloudDir;
+	private String LocalDir;
+
+	public static void main(String[] args) {
+
+		//setConfig("config.ini");
+		
+		BackupNotes backup = new BackupNotes();
+		
+		Properties properties = new Properties();
+
+		try {
+			properties.load(new FileInputStream("src/paths.properties"));
+		} catch (IOException e){
+			System.out.println(e);
+		}
+
+		backup.setConfig("src/paths.properties");		
+		backup.getAvailableBackups(backup);
+		System.out.println();
+		
+	}
+	
+	private void setConfig(String configPath) {
+		Properties properties = new Properties();
+		
+		try {
+			properties.load(new FileInputStream("src/paths.properties"));
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		
+		this.BackupsDir = properties.getProperty("backups_dir");
+		this.pCloudDir = properties.getProperty("pcloud_dir");
+		this.LocalDir = properties.getProperty("local_dir");
+
+	}
+	
+	private void getAvailableBackups(BackupNotes backup) {
+		File backupsDir = new File(backup.BackupsDir);
+		
+		//to do: refactor with try/catch block
+		if(!backupsDir.exists() && !backupsDir.isDirectory()) {
+			String ErrorMessage = "Please attach the backup storage!";
+			System.out.println(ErrorMessage);
+			System.exit(0);
+		}
+		
+		File[] availableBackups = backupsDir.listFiles();
+		
+		for (File f : availableBackups) {
+			System.out.println(f.getName());
+		}
+	}
+	
+	private String generateDateString() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+	
+}
